cc7376c057331c6b1ecce223990780ef4b47f971
Janet Warren <jdublu416@gmail.com>
2018-04-29 15:00:08 -0400

    remove node-js file? bug fixes to friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,53 @@
 var friends = [
   {
     name: "Sheila",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [5, 1, 4, 4, 5, 1, 2, 4, 3, 2]
   },
 
   {
     name: "June",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 4, 3, 3, 5, 2, 3, 4, 1, 3]
   },
   {
     name: "Jackie",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 4]
   },
   {
     name: "Ella",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [5, 2, 3, 4, 5, 4, 3, 3, 1, 4]
   },
   {
     name: "Bob",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 3, 3, 4, 5, 4, 3, 4, 1, 4]
   },
   {
     name: "Alice",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 1, 3, 4, 5, 4, 2, 2, 1, 4]
   },
   {
     name: "Phillip",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 4, 3, 4, 5, 4, 3, 5, 1, 4]
   },
   {
     name: "Heather",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [1, 2, 4, 3, 5, 3, 4, 2, 1, 3]
   },
   {
     name: "Ryan",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL:"https://picsum.photos/300/450/?random",
     scores: [2, 1, 3, 4, 5, 4, 3, 1, 2, 4]
   },
   {
     name: "Kathy",
-    photoURL: "https://picsum.photos/200/300/?random",
+    photoURL: "https://picsum.photos/300/450/?random",
     scores: [3, 2, 1, 4, 5, 4, 1, 2, 3, 4]
   }
 ];
